6bb9ff577b3eae6b6669b8d1d9f45300f4dc0c99
arinagomza <a11r221nir3@gmail.com>
2025-05-25 17:08:16 +0300

    feat(DataAccess): добавлена модель TaskItem

---
+++ TaskManager.DataAccess/Models/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager.DataAccess.Models
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsDone { get; set; }
+    }
+}
